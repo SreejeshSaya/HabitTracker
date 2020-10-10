2d33086e67fe6df8c61d1023c40a4c855c21a124
lord8266 <lordvishwa123@gmail.com>
2020-10-10 15:46:11 +0530

    add baseline profile

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { SignupComponent } from './signup/signup.component';
 import {PendingComponent} from './pending/pending.component'
 import { AddHabitComponent } from './add-habit/add-habit.component';
 import { HabitManageComponent } from './habit/habit-manage/habit-manage.component';
+import {UserDetailsComponent }from './user-details/user-details.component'
 
 const routes: Routes = [
    {path:"",component:PendingComponent,pathMatch:"full"},
    {path:'login',component:LoginComponent},
    {path:'signup',component:SignupComponent},
    {path:'add-habit',component:AddHabitComponent},
-   {path:'habits/:habitId',component:HabitManageComponent}
+   {path:'habits/:habitId',component:HabitManageComponent},
+   {path:'profile',component:UserDetailsComponent}
 ];
 
 @NgModule({
--- client/src/app/user-details/user-details.component.ts
+++ client/src/app/user-details/user-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter, map, switchMap } from 'rxjs/operators';
+import { AuthService } from '../auth.service';
+import { HabitService } from '../habit.service';
 
 @Component({
   selector: 'app-user-details',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-details.component.css']
 })
 export class UserDetailsComponent implements OnInit {
+   routeSub;
+   authSub;
+   constructor(public authService: AuthService, private router: Router, public habitService: HabitService, route: ActivatedRoute) {
+      this.routeSub = route.params.subscribe(val => {
+         if (!this.authService.userDetails) {
+            this.authService.getUserDetails();
+         }
+      });
+   }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+   ngOnInit(): void {
+      this.authSub = this.authService.isLoading$.pipe(map(l => {
+         if (!l && this.authService.userDetails) {
+            return true
+         } else if (!l) {
+            this.router.navigateByUrl('/login');
+         }
+         else {
+            return false;
+         }
+      }),filter(l=>l),switchMap(_=>{
+         return this.habitService.isLoading$
+      }))
+      .subscribe(l=>{
+         if (!l && this.habitService.userHabits) {
+            // this.userHabits = this.habitService.userHabits
+            console.log(this.habitService.userHabits)
+         } else if (!l) {
+            this.router.navigateByUrl('/');
+         }
+      });
+   }
+   
+   ngOnDestroy(){
+      this.authSub.unsubscribe();
+      this.routeSub.unsubscribe();
+   }
 
 }
