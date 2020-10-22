d81f792f80665b4e1eb0387873c932297ee69b6a
SreejeshSaya <40210225+SreejeshSaya@users.noreply.github.com>
2020-10-22 20:18:56 +0530

    server-side email and username Validation
    Added middleware functions for email and username validation while logging and signing up.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,146 +1,164 @@
 const User = require("../models/user")
-const Habit = require("../models/habit")
-const validator = require('validator')
-const bcrypt = require("bcrypt");
-// TODO Validation
-// TODO Error Handling
+// const Habit = require("../models/habit")
+const validator = require("validator")
+const bcrypt = require("bcrypt")
 
-// Username normal function
-exports.usernameValidation = async(req, res, next) => {
-    message = '';
-    charLimit = 25;
-    username = req.body.username
-    if(validator.isEmpty(username)) {
-        message = "Username Required"
-    }
-    else if(username.length > charLimit) {
-        message = "Username greater than {charLimit} characters!"
-
-    }
-    else if(validator.matches(username, '^[a-zA-Z0-9]*\_?[a-zA-Z0-9]+$')) {
-        const user = await User.findOne({ username: username });
-        if(user) {
-            // Middleware function or a normal function?
-            message = 'UserName Exists!'
-
-        }
-        else {
-            next();
-        }
-    }
-    if(message.length > 0)
-        res.status(401).send(message)
-    else
-        next()
-    // res.send(message)
+exports.usernameValidation = async (req, res, next) => {
+	message = ""
+	charLimit = 25
+	username = req.body.username
+	if (validator.isEmpty(username)) {
+		message = "Username Required"
+	} else if (username.length > charLimit) {
+		message = `Username greater than ${charLimit} characters!`
+	} else if (validator.matches(username, "^[a-zA-Z0-9]*_?[a-zA-Z0-9]+$")) {
+		const user = await User.findOne({ username: username })
+		if (user) {
+			// Middleware function or a normal function?
+			message = "UserName Exists!"
+		} else {
+			console.log(`Username ${username} is valid`)
+			next()
+		}
+	} else {
+		message = "Username Invalid"
+	}
+	res.send(message)
+	// if (message.length > 0) res.status(401).send(message)
+	// else next()
+	// res.send(message)
 }
 
-exports.emailValidation = async(req, res, next) => {
-    message = ''
-    emailID = req.body.email
-    if(validator.isEmpty(email)) {
-        message = "Field must not be left Empty!"
-        res.status(401)
-    }
-    else if(validator.isEmail(email)) {
-        const user = await User.findOne({ email: email });
-        if(user) {
-            message = 'An account already exists with this email ID'
-            res.status(401)
-        }
-        else {
-            message = 'Available'
-            res.status(200)
-        }
-    }
-    else {
-        message = "Email ID invalid"
-        res.status(401)
-    }
-    res.send(message)
+exports.emailValidation = async (req, res, next) => {
+	message = ""
+	emailId = req.body.email
+	if (validator.isEmpty(emailId)) {
+		message = "Email Id Empty!"
+	} else if (validator.isEmail(emailId)) {
+		console.log("Email Valid")
+		// next()
+		const user = await User.findOne({ email: email })
+		if (user) {
+			message = "An account already exists with this email ID"
+			res.status(401)
+		} else {
+			next()
+		}
+	} else {
+		message = "Email ID invalid"
+	}
+	res.status(401)
+	res.send(message)
 }
 
-exports.requireAuth = async (req,res,next)=>{
-    if (req.session.userId){
-        next()
-    }
-    else {
-        res.status(401).send("Require Authentication")
-    }
+exports.loginValidation = async (req, res, next) => {
+	message = ""
+	emailId = req.body.email
+	console.log(emailId)
+	if (validator.isEmpty(emailId)) {
+		message = "Field must not be left Empty!"
+	} else if (validator.isEmail(emailId)) {
+		console.log("Email Valid")
+		next()
+		// const user = await User.findOne({ email: email })
+		// if (user) {
+		// 	message = "An account already exists with this email ID"
+		// 	res.status(401)
+		// } else {
+		// 	message = "Available"
+		// 	res.status(200)
+		// }
+	} else {
+		message = "Email ID invalid"
+		console.log(message)
+	}
+	res.status(401)
+	res.send(message)
 }
 
-exports.isLoggedIn = async (req,res,next)=>{
-    res.setHeader('Content-Type', 'text/plain');
-    if (req.session.userId){
-        res.status(401)
-        .send("Yes");
-    }
-    else {
-        res.status(401)
-        .send("No");
-    }
+exports.requireAuth = async (req, res, next) => {
+	if (req.session.userId) {
+		next()
+	} else {
+		res.status(401).send("Require Authentication")
+	}
 }
 
-exports.getUserDetails = async (req,res,next)=>{
-    const user = await User.findById(req.session.userId)
-    res.send({ username:user.username,email: user.email,userId:user._id,createdAt:user.createdAt })
+exports.isLoggedIn = async (req, res, next) => {
+	res.setHeader("Content-Type", "text/plain")
+	if (req.session.userId) {
+		res.status(401).send("Yes")
+	} else {
+		res.status(401).send("No")
+	}
 }
 
-exports.updateUserDetails = async (req,res,next)=>{
-    const user = await User.findById(req.session.userId)
-    if (!user){
-        return {
-            error: 'User not found'
-        }
-    }
-    else {
-        console.log("recvvvv",req.body.username)
-        if (req.body.username){
-            user.username = req.body.username
-        }
-    }
-    await user.save()
-    res.send({ username:user.username,email: user.email,userId:user._id,createdAt:user.createdAt })
+exports.getUserDetails = async (req, res, next) => {
+	const user = await User.findById(req.session.userId)
+	res.send({
+		username: user.username,
+		email: user.email,
+		userId: user._id,
+		createdAt: user.createdAt,
+	})
 }
 
-exports.signup = async (req,res,next)=>{
-    const username = req.body.username
-    const password = req.body.password
-    const email = req.body.email
-    const passwordHash = await bcrypt.hash(password,12)
-    const user = new User({
-        username:username,
-        password:passwordHash,
-        email:email
-    })
-    await user.save()
-    req.session.userId = user._id
-    res.send("Ok") 
+exports.updateUserDetails = async (req, res, next) => {
+	const user = await User.findById(req.session.userId)
+	if (!user) {
+		return {
+			error: "User not found",
+		}
+	} else {
+		console.log("recvvvv", req.body.username)
+		if (req.body.username) {
+			user.username = req.body.username
+		}
+	}
+	await user.save()
+	res.send({
+		username: user.username,
+		email: user.email,
+		userId: user._id,
+		createdAt: user.createdAt,
+	})
 }
 
-exports.login = async (req,res,next)=>{
-    const email = req.body.email
-    const password = req.body.password
-    const user = await User.findOne({
-        email:email
-    })
-    if (user){
-        console.log(user)
-        if (await bcrypt.compare(password,user.password)){
-            req.session.userId = user._id
-            res.send("Ok")
-        }
-        else {
-            res.status(401).send("Invalid Password")
-        }
-    }
-    else{
-        res.status(401).send("Email Not Found")
-    }
+exports.signup = async (req, res, next) => {
+	const username = req.body.username
+	const password = req.body.password
+	const email = req.body.email
+	const passwordHash = await bcrypt.hash(password, 12)
+	const user = new User({
+		username: username,
+		password: passwordHash,
+		email: email,
+	})
+	await user.save()
+	req.session.userId = user._id
+	res.send("Ok")
 }
 
-exports.logout =  async (req,res,next)=>{
-    req.session.destroy()
-    res.send("Ok")
+exports.login = async (req, res) => {
+	const email = req.body.email
+	const password = req.body.password
+	const user = await User.findOne({
+		email: email,
+	})
+	if (user) {
+		console.log(user)
+		if (await bcrypt.compare(password, user.password)) {
+			req.session.userId = user._id
+			res.send("Ok")
+		} else {
+			res.status(401).send("Invalid Password")
+		}
+	} else {
+		res.status(401).send("Email Not Found")
+	}
 }
 
+exports.logout = async (req, res, next) => {
+	req.session.destroy()
+	res.send("Ok")
+}
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,4 @@
-const {Router} = require("@awaitjs/express")
+const { Router } = require("@awaitjs/express")
 
 const router = Router()
 const authControllers = require("../controllers/auth")
@@ -7,11 +7,33 @@ const authControllers = require("../controllers/auth")
 // TODO Error Handling
 // TODO Use PassportJS for authentication
 
-router.getAsync("/is-logged-in",authControllers.isLoggedIn)
-router.getAsync("/get-user-details",authControllers.requireAuth,authControllers.getUserDetails)
-router.postAsync("/update-user-details",authControllers.requireAuth,authControllers.updateUserDetails)
-router.postAsync("/signup",authControllers.signup)
-router.postAsync("/login",authControllers.login)
-router.postAsync("/logout",authControllers.logout)
+router.getAsync("/is-logged-in", authControllers.isLoggedIn)
+
+router.getAsync(
+	"/get-user-details",
+	authControllers.requireAuth,
+	authControllers.getUserDetails
+)
+
+router.postAsync(
+	"/update-user-details",
+	authControllers.requireAuth,
+	authControllers.updateUserDetails
+)
+
+router.postAsync(
+	"/signup",
+	authControllers.usernameValidation,
+	authControllers.emailValidation,
+	authControllers.signup
+)
+
+router.postAsync(
+	"/login",
+	authControllers.loginValidation,
+	authControllers.login
+)
+
+router.postAsync("/logout", authControllers.logout)
 
 module.exports = router
