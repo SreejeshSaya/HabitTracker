5970f8683d610e876816c748c6f98b47e6bc2f51
SreejeshSaya <40210225+SreejeshSaya@users.noreply.github.com>
2020-11-03 19:34:43 +0530

    Fixed streakCalendar Component

--- client/src/app/habit/habit-manage/habit-manage.component.ts
+++ client/src/app/habit/habit-manage/habit-manage.component.ts
@@ -4,9 +4,9 @@ import { AuthService } from 'src/app/auth.service';
 import { HabitService } from 'src/app/habit.service';
 import { concatMap, tap, map, filter, switchMap } from 'rxjs/operators';
 import { of } from 'rxjs';
-import { getDateString,getHistory, removeTime } from 'src/app/dateManager'
-import {colors} from 'src/app/colors'
-import {makePunctualityGraph,makeStreakGraph} from 'src/app/grapher'
+import { getDateString, getHistory, removeTime } from 'src/app/dateManager';
+import { colors } from 'src/app/colors';
+import { makePunctualityGraph, makeStreakGraph } from 'src/app/grapher';
 
 @Component({
    selector: 'app-habit-manage',
@@ -24,7 +24,7 @@ export class HabitManageComponent implements OnInit {
    selectedColor;
    colors;
    histStreak = [];
-   message = "test";
+   message = 'test';
    sdata;
    pdata;
    constructor(
@@ -33,79 +33,94 @@ export class HabitManageComponent implements OnInit {
       public authService: AuthService,
       public habitService: HabitService
    ) {
-      this.routeSub = route.params.subscribe(val => {
-         this.habitId = +val['habitId']
+      this.routeSub = route.params.subscribe((val) => {
+         this.habitId = +val['habitId'];
          if (!this.authService.userDetails) {
             this.authService.getUserDetails();
          }
       });
-      this.colors =colors
+      this.colors = colors;
    }
 
    ngOnInit(): void {
-      this.authSub = this.authService.isLoading$.pipe(map(l => {
-         if (!l && this.authService.userDetails) {
-            return true
-         } else if (!l) {
-            this.router.navigateByUrl('/login');
-         }
-         else {
-            return false;
-         }
-      }),filter(l=>l),switchMap(_=>{
-         return this.habitService.isLoading$
-      }))
-      .subscribe(l=>{
-         if (!l && this.habitService.userHabits && this.habitService.userHabits[this.habitId]) {
-            this.habit = this.habitService.userHabits[this.habitId]
-            this.text = this.habit.text
-            this.endDate = getDateString(this.habit.endDate)
-            this.history = getHistory(this.habit.createdAt,this.habit.history)
-            this.selectedColor = this.habit.color
-            this.createStreakCalendar();
-            this.pdata = makePunctualityGraph(this.habit,this.habit.history,90)
-            this.sdata = makeStreakGraph(this.habit,this.habit.history,90)
-         } else if (!l) {
-            this.router.navigateByUrl('/');
-         }
-      });
+      this.authSub = this.authService.isLoading$
+         .pipe(
+            map((l) => {
+               if (!l && this.authService.userDetails) {
+                  return true;
+               } else if (!l) {
+                  this.router.navigateByUrl('/login');
+               } else {
+                  return false;
+               }
+            }),
+            filter((l) => l),
+            switchMap((_) => {
+               return this.habitService.isLoading$;
+            })
+         )
+         .subscribe((l) => {
+            if (
+               !l &&
+               this.habitService.userHabits &&
+               this.habitService.userHabits[this.habitId]
+            ) {
+               this.habit = this.habitService.userHabits[this.habitId];
+               this.text = this.habit.text;
+               this.endDate = getDateString(this.habit.endDate);
+               this.history = getHistory(
+                  this.habit.createdAt,
+                  this.habit.history
+               );
+               this.selectedColor = this.habit.color;
+               this.createStreakCalendar();
+               this.pdata = makePunctualityGraph(
+                  this.habit,
+                  this.habit.history,
+                  90
+               );
+               this.sdata = makeStreakGraph(this.habit, this.habit.history, 90);
+            } else if (!l) {
+               this.router.navigateByUrl('/');
+            }
+         });
    }
 
-   updateHabit(){
-      this.habitService.updateHabit(this.habit.index,this.text,this.selectedColor,this.endDate)
-      .subscribe(d=>{
-         this.router.navigateByUrl("/")
-      })
+   updateHabit() {
+      this.habitService
+         .updateHabit(
+            this.habit.index,
+            this.text,
+            this.selectedColor,
+            this.endDate
+         )
+         .subscribe((d) => {
+            this.router.navigateByUrl('/');
+         });
    }
 
    createStreakCalendar() {
       let monthStreak = [];
       const d = removeTime(new Date());
       let month = d.getMonth();
-      let i = this.history.length-1;
-      let j=0;
-      while(j<3) { // j indicated the number of months seen
-         while(i>=0) { // stop when reached end
-            if(this.history[i].date.getMonth() === month) { 
-               monthStreak.push(this.history[i])
+      let i = this.history.length - 1;
+      console.log(this.history);
+      let j = 0;
+      while (j < 3) {
+         while (i >= 0) {
+            if (this.history[i].date.getMonth() === month) {
+               monthStreak.push(this.history[i]);
+            } else {
+               break;
             }
-            // else { // stop when month has changed
-            //    break;
-            // }
             i--;
          }
-         this.histStreak.push({month: month, monthStreak: monthStreak});
+         this.histStreak.push({ month: month, monthStreak: monthStreak });
          monthStreak = [];
          j++;
-         month = new Date(d.setMonth(month-1)).getMonth();       
+         month = new Date(d.setMonth(month - 1)).getMonth();
       }
       this.histStreak.reverse();
-      console.log(this.histStreak)
-
-      // this.histStreak.forEach((streak) => {
-      //    console.log("Month" + streak.month)
-      //    console.log("monthStreak" + streak.monthStreak)
-      // })
    }
 
    ngOnDestroy() {
@@ -113,17 +128,14 @@ export class HabitManageComponent implements OnInit {
       this.authSub.unsubscribe();
    }
 
-   deleteHabit(){
-      this.habitService.deleteHabit(this.habit.index)
-      .subscribe(d=>{
-         this.router.navigateByUrl("/")
-      })
+   deleteHabit() {
+      this.habitService.deleteHabit(this.habit.index).subscribe((d) => {
+         this.router.navigateByUrl('/');
+      });
    }
-   colorChange(color){
-      this.selectedColor = color 
+   colorChange(color) {
+      this.selectedColor = color;
    }
 
-   makeStreakGraph(){
-      
-   }
+   makeStreakGraph() {}
 }
